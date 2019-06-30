d570daee3aa98e16dbcd0112c9051e49a304d672
dnoulet <daniel.noulet@gmail.com>
2019-06-30 20:03:59 +0200

    versioning

--- hello-world/src/main/java/be/urpi/software/xsi/node/helloworld/controller/HelloWorldController.java
+++ hello-world/src/main/java/be/urpi/software/xsi/node/helloworld/controller/HelloWorldController.java
@@ -1,15 +1,18 @@
 package be.urpi.software.xsi.node.helloworld.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RequestMapping(path = "/helloWorld")
 @RestController
 public class HelloWorldController {
     @GetMapping
-    ResponseEntity<String> helloWorld(){
+    ResponseEntity<String> helloWorld() {
+        log.info("Hit helloWorld endpoint");
         return ResponseEntity.ok("Hello World");
     }
 }
